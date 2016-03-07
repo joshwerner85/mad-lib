dc39c5502243ce3c8b994f6624ac4133bc1de3ab
Josh <you@example.coexit>
2016-03-07 14:19:06 -0600

    Mad-Lib version 2

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var noun = prompt ('Enter a noun');
+
+var verb = prompt ('Enter a verb');
+
+var adjective = prompt ('Enter a adjective');
+
+var adverb = prompt ('Enter a adverb');
+
+ 
+
+
+//console.log('Do you '+verb+' your '+adjective+' '+noun+' '+adverb+'? That\'s hilarious');
+
+document.write('Do you '+verb+' your '+adjective+' '+noun+' '+adverb+'? That\'s hilarious');
+//console.log('You will be '+nounFirst+' years old a year from now');
+
+ 
